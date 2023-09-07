85b21eb9ecb61dbb1375a8507de3c6f5c40ce9e7
Ahmed Hani Atef <ahmedhaniatef@gmail.com>
2023-09-07 16:08:18 +0300

    create documentaion for the interface of the realy

--- ECU_Layer/ecu_relay/ecu_relay.c
+++ ECU_Layer/ecu_relay/ecu_relay.c
@@ -5,15 +5,20 @@
  * @date 2023-09-07
  */
 
-
 /****************** Start of Section : includes ********************/
 #include "ecu_relay.h"
 /******************* End of Section : includes *********************/
 
-
 /************ Start of Section : function declaration **************/
 
-std_returntype relay_initialize (const relay_config_t *__relay)
+/**
+ * @brief initialize the pin of relay as output 
+ * @param __relay a pointer to the relay want to be initialized
+ * @return std_returntype
+ * @retval (STD_OK)     : if the function run successfully
+ * @retval (STD_NOT_OK) : if something goes wrong
+ */
+std_returntype relay_initialize(const relay_config_t *__relay)
 {
     std_returntype ret = STD_OK;
     if (NULL == __relay)
@@ -25,10 +30,17 @@ std_returntype relay_initialize (const relay_config_t *__relay)
         pin_cofig_t __pin = {.port = __relay->port, .pin = __relay->pin, .direction = GPIO_INPUT, .logic = __relay->status};
         ret = gpio_pin_initialize(&__pin, GPIO_INPUT, __relay->status);
     }
-    return  ret;
+    return ret;
 }
 
-std_returntype relay_turn_on (const relay_config_t *__relay)
+/**
+ * @brief turn on the relay
+ * @param __relay a pointer to the relay want to be on
+ * @return std_returntype
+ * @retval (STD_OK)     : if the function run successfully
+ * @retval (STD_NOT_OK) : if something goes wrong
+ */
+std_returntype relay_turn_on(const relay_config_t *__relay)
 {
     std_returntype ret = STD_OK;
     if (NULL == __relay)
@@ -40,10 +52,17 @@ std_returntype relay_turn_on (const relay_config_t *__relay)
         pin_cofig_t __pin = {.port = __relay->port, .pin = __relay->pin, .direction = GPIO_INPUT, .logic = __relay->status};
         ret = gpio_pin_write(&__pin, GPIO_HIGH);
     }
-    return  ret;
+    return ret;
 }
 
-std_returntype relay_turn_off (const relay_config_t *__relay)
+/**
+ * @brief turn off the relay
+ * @param __relay a pointer to the relay want to be off
+ * @return std_returntype
+ * @retval (STD_OK)     : if the function run successfully
+ * @retval (STD_NOT_OK) : if something goes wrong
+ */
+std_returntype relay_turn_off(const relay_config_t *__relay)
 {
     std_returntype ret = STD_OK;
     if (NULL == __relay)
@@ -55,12 +74,10 @@ std_returntype relay_turn_off (const relay_config_t *__relay)
         pin_cofig_t __pin = {.port = __relay->port, .pin = __relay->pin, .direction = GPIO_INPUT, .logic = __relay->status};
         ret = gpio_pin_write(&__pin, GPIO_LOW);
     }
-    return  ret;
+    return ret;
 }
 
 /************ End of Section :  function declaration ***************/
-
-
 
 /********************************************************************************
  * log
@@ -68,5 +85,5 @@ std_returntype relay_turn_off (const relay_config_t *__relay)
  * author               date            details
  * ******************************************************************************
  * Ahmed Hani Atef      07-09-2023      - create the file
- * Ahmed Hani Atef      07-09-2023      - creating the interface of led 
+ * Ahmed Hani Atef      07-09-2023      - creating the interface of led
  */
